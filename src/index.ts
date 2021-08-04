04f0fb9c8cabe184e8e340b6e191cba051ed5b9c
Ken Ho <kentsu.ho@gmail.com>
2021-08-04 23:00:25 +0800

    fix: if, if-branch and v-for should be removed, close #1

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,36 @@ module.exports = function (context: IPluginContext) {
     // && framework !== context.helper.FRAMEWORK_MAP.VUE
   ) return
 
+  function findEnv (source: string) {
+    const envReg = /(?<=(taro-env|taroEnv)=")([a-z0-9]+)(?=")/g
+    const found = source.match(envReg)
+    return found !== null ? found[0] : found
+  }
+
+  function isTaroEnv (propName: string) {
+    return propName === 'taro-env' || propName === 'taroEnv'
+  }
+
   /**
-   * Transform elements with `taro-env` attribute.
+   * Transform elements with `taro-env` or `taroEnv` attribute.
    * <view taro-env="weapp">weapp specific node</view>
+   * <view taroEnv="h5">h5 specific node</view>
    */
   function transformTaroEnv (node, ctx) {
-    if (node.type === 1 /* ELEMENT */) {
+    // if, if-branch, v-for
+    if (node.type >= 9 && node.type <= 11) {
+      const source = node.type === 11
+        ? node.codegenNode.loc.source
+        : node.loc.source
+
+      const targetEnv = findEnv(source)
+
+      if (targetEnv !== process.env.TARO_ENV) {
+        ctx.removeNode(node)
+      }
+    } else if (node.type === 1 /* ELEMENT */) {
       node.props.forEach((prop, index) => {
-        if (prop.type === 6 && prop.name === 'taro-env') {
+        if (prop.type === 6 && isTaroEnv(prop.name)) {
           process.env.TARO_ENV !== prop.value.content
             ? ctx.removeNode(node)
             : node.props.splice(index, 1)
